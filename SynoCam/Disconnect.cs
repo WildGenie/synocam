580e5e724701eef751f32cbb67252734190005ad
sndrwie <git@wiering.cc>
2016-01-04 20:07:52 +0100

    Forgot file

---
+++ SynoCam/Disconnect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SynoCamLib;
+
+namespace SynoCam
+{
+    public partial class Disconnect : Form
+    {
+        private readonly SynoCommand _synoCommand;
+
+        public Disconnect(SynoCommand synoCommand)
+        {
+            _synoCommand = synoCommand;
+            InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private void OnShown(object sender, EventArgs eventArgs)
+        {
+            _synoCommand.LogoutASync().ContinueWith(ExitAfterLogout);
+        }
+
+        private void ExitAfterLogout(Task task)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
