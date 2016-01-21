590ae812dfadf072cda8e4d662a6be9266714142
sndrwie <git@wiering.cc>
2016-01-21 16:35:11 +0100

    Adding the first starter for a WPF based version of SynoCam

---
+++ SynoCamWPF/CamControl.cs
@@ -0,0 +1,22 @@
+﻿using SynoCamLib;
+
+namespace SynoCamWPF
+{
+    public class CamControl : ICam
+    {
+        public string CamName { get; private set; }
+        public CamStatus Status { get; private set; }
+        public bool Enabled { get; private set;  }
+        public string Url { get; private set; }
+        public RefreshRate RefreshRate { get; private set; }
+
+        public CamControl(ICam cam, RefreshRate refreshRate)
+        {
+            CamName = cam.CamName;
+            Status = cam.Status;
+            Enabled = cam.Enabled;
+            Url = cam.Url;
+            RefreshRate = refreshRate;
+        }
+    }
+}
---
+++ SynoCamWPF/RefreshRate.cs
@@ -0,0 +1,11 @@
+﻿namespace SynoCamWPF
+{
+    public enum RefreshRate
+    {
+        Ms4Minutes = 240000,
+        Ms2Minutes = 120000,
+        Ms1Minute = 60000,
+        Ms30Seconds = 30000,
+        Ms2Seconds = 2000
+    }
+}
